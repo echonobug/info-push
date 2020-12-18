39037fcec5013e58b3bf270289c5b3f1ad702197
jwei <466944168@qq.com>
2020-12-19 01:00:55 +0800

    补上次提交

---
+++ info-push-web/src/main/java/ipush/config/log/access/AccessLogInterceptor.java
@@ -0,0 +1,70 @@
+package ipush.config.log.access;
+
+import ipush.entity.AccessLog;
+import ipush.entity.User;
+import ipush.mapper.AccessLogMapper;
+import ipush.util.IpUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Component
+public class AccessLogInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private AccessLogMapper accessLogMapper;
+
+    /**
+     * 请求开始时间标识
+     */
+    private static final String LOGGER_SEND_TIME = "SEND_TIME";
+    /**
+     * 请求日志实体标识
+     */
+    private static final String LOGGER_ACCESS_LOG = "ACCESS_LOG_ENTITY";
+
+    /**
+     * 未登录用户
+     */
+    private static final String GUEST_USER_NAME = "未登录用户";
+
+    /**
+     * 进入Controller之前开始记录日志实体
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) {
+        AccessLog accessLog = new AccessLog();
+        accessLog.setRequestIp(IpUtil.getIpAdrress(request));
+        accessLog.setRequestMethod(request.getMethod());
+        accessLog.setRequestUrl(request.getRequestURI());
+        request.setAttribute(LOGGER_SEND_TIME, System.currentTimeMillis());
+        request.setAttribute(LOGGER_ACCESS_LOG, accessLog);
+        return true;
+    }
+
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object o, Exception e) {
+        AccessLog accessLog = (AccessLog) request.getAttribute(LOGGER_ACCESS_LOG);
+        int status = response.getStatus();
+        accessLog.setResponseStatus(status);
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof User) {
+            accessLog.setUserName(((User) principal).getUsername());
+        } else {
+            accessLog.setUserName(GUEST_USER_NAME);
+        }
+        long currentTime = System.currentTimeMillis();
+        long snedTime = Long.parseLong(request.getAttribute(LOGGER_SEND_TIME).toString());
+        accessLog.setRequestDuration(currentTime - snedTime);
+        accessLogMapper.insertSelective(accessLog);
+        log.info(accessLog.toString());
+    }
+}
+
---
+++ info-push-web/src/main/java/ipush/service/AccessLogService.java
@@ -0,0 +1,16 @@
+package ipush.service;
+
+import ipush.entity.AccessLog;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 服务类
+ * </p>
+ *
+ * @author jwei
+ * @since 2020/12/18
+ */
+public interface AccessLogService extends IService<AccessLog> {
+	
+}
